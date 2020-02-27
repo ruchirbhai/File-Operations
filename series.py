fb9d8839c32bef140124530dd807fa283c129355
ruchirbhai <ruchirbhai@gmail.com>
2020-02-26 18:44:32 -0800

    refactor with comprehension
    change code to use comprehension

--- series.py
+++ series.py
@@ -4,15 +4,16 @@ import sys
 
 def read_series(filename):
     try:
-        series = []
+        #series = []
         f = open(filename, mode='rt', encoding='utf-8')
-        for line in f:
-            a = int(line.strip())
-            series.append(a)
+        #for line in f:
+        #    a = int(line.strip())
+        #    series.append(a)
+        #refactor Code to use comprehension
+        return [int(line.strip()) for line in f]
+
     finally:
         f.close()
-    return series
-
 
 def main(filename):
     series = read_series(filename)
