73ae21f615c9a9df5717aad3053c44496f71816b
ruchirbhai <ruchirbhai@gmail.com>
2020-02-26 18:47:09 -0800

    refactor cleanup

--- series.py
+++ series.py
@@ -4,12 +4,8 @@ import sys
 
 def read_series(filename):
     try:
-        #series = []
-        f = open(filename, mode='rt', encoding='utf-8')
-        #for line in f:
-        #    a = int(line.strip())
-        #    series.append(a)
         #refactor Code to use comprehension
+        f = open(filename, mode='rt', encoding='utf-8')
         return [int(line.strip()) for line in f]
 
     finally:
